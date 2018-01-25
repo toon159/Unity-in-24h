2e2581e29f897f07ced03858ec151ebf18ad41a3
toon159 <toonbcc@gmail.com>
2018-01-25 19:15:04 +0700

    update repo
    last push was not correct

---
+++ Assets/_Scripts/AnimationControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour {
+
+	private Animator anim;
+	void Start () {
+		//Get a reference to the animator
+		anim = GetComponent<Animator> ();
+	}
+	void Update () {
+		anim.SetFloat ("Speed", Input.GetAxis ("Vertical"));
+		anim.SetFloat ("Direction", Input.GetAxis( "Horizontal"));
+	}
+}
