5604f42c6e097855a81b6fcd15190ae58673ca33
toon159 <toonbcc@gmail.com>
2018-01-27 23:56:50 +0700

    Add power-up and ground moving
    -still buggy

---
+++ Assets/_Scripts/Ground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+	public float speed = .5f;
+
+	private Renderer groundRenderer;
+
+	// Use this for initialization
+	void Start () {
+		groundRenderer = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = Time.time * speed % 1;  
+		groundRenderer.material.mainTextureOffset = new Vector2 (0, -offset);
+	}
+
+	public void SlowDown(){
+		speed = speed / 2;
+	}
+
+	public void SpeedUp(){
+		speed = speed * 2;
+	}
+}
